141c6b80a59d46b40e69131754afa15137debc31
Korey Peterson <korey.t.peterson@gmail.com>
2019-12-13 15:59:53 -0600

    components prepped

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 import './App.css';
 
 class App extends Component {
@@ -11,6 +11,31 @@ class App extends Component {
           <h4><i>Don't forget it!</i></h4>
         </header>
         <br/>
+        {/* Below is Step 1 Comp*/}
+        <form>
+          <h2> Step 1: How are you feeling today?</h2>
+          <input type="number" placeholder="From 1 to 5" />
+          <button>Next</button>
+        </form>
+        {/* Below is Step 2 Comp*/}
+        <form>
+          <h2> Step 2: How well are you understanding the content?</h2>
+          <input type="number" placeholder="From 1 to 5" />
+          <button>Next</button>
+        </form>
+        {/* Below is Step 3 Comp*/}
+        <form>
+          <h2> Step 3: How well are you being supported?</h2>
+          <input type="number" placeholder="From 1 to 5" />
+          <button>Next</button>
+        </form>
+         {/* Below is Step 4 Comp*/}
+         <form>
+          <h2> Step 4: Any additional comments you want to leave?</h2>
+          <input type="text" placeholder="From 1 to 5" />
+          <button>Next</button>
+        </form>
+
       </div>
     );
   }
